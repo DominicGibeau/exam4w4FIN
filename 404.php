7dfbe1c6f445d7a28a0545cdf5427a1b3a7cd197
DominicGibeau <DominicGibeau>
2024-04-10 08:56:34 -0400

    s9cl premier commit du tp2

---
+++ 404.php
@@ -0,0 +1,34 @@
+
+
+<?php get_header(); ?>
+
+
+        <div id="erreur_404" class="global">
+            <section>
+                <h2>erreur 404</h2>
+<p>vous essayer d'acceder a une page qui n'existe pas</p>
+
+<?php get_bloginfo("name"); get_bloginfo('url') ?>
+
+        <div id="footer" class="global">
+
+         
+
+            <footer>
+                <h2>Footer</h2>
+              
+                <a href="https://www.youtube.com/channel/UCtaLgfIokNyxrX03heg888g">voici mon youtube</a>
+             
+            </footer>
+
+
+            <div class="vague">
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                    <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill" style="fill: var(--couleur-arriere-galerie);"></path>
+                </svg>
+            </div>
+            
+
+        </div>
+    </body>
+</html>
