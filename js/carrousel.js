314eacf9d981ff3717eef9ce397e3ec1b5c4d43a
DominicGibeau <DominicGibeau>
2024-05-16 10:33:56 -0400

    push de debut de cour

--- js/carrousel.js
+++ js/carrousel.js
@@ -30,14 +30,98 @@ function creer_image_carrousel
 
     for (const elm of galerie__imgs) {
 
+
+        elm.dataset.index = index
+
+
 creer_image_carrousel(elm, index)
 creer_radio_carrousel(index)
+
+  //ajouter un event listener sur les image
+  elm.addEventListener('click', function(){
+    carrousel.classList.add('carrousel--ouvrir');
+    console.log(elm.dataset.index);
+    carrousel__radios[elm.dataset.index].checked = true;
+    activerLaBonneImg(elm.dataset.index);
+});
+
 index = index + 1
     }
 
+   //aller chercher tt les img du carrousel
+   let carrousel__radios = document.querySelectorAll('.carrousel__radio');
+   //mettre le premier radio en checked
+   //carrousel__radios[0].checked = true;
+   //mettre la première image en opacité 1 et les autres en 0
+   let carrousel__imgs = carrousel__figure.querySelectorAll('.carrousel__img');
+   for (const img of carrousel__imgs) {
+       img.style.opacity = 0;
+   }
+   carrousel__imgs[0].style.opacity = 1;
+   
 
-
-
+ 
+    //création de 2 btn pour changer d'image vers la droite ou vers la gauche portant la classe carrousel__fleche
+    let carrousel__fleche__gauche = document.createElement('button');
+    carrousel__fleche__gauche.classList.add('carrousel__fleche');
+    carrousel__fleche__gauche.classList.add('carrousel__fleche--gauche');
+    carrousel__fleche__gauche.innerHTML = '<';
+    
+    let carrousel__fleche__droite = document.createElement('button');
+    carrousel__fleche__droite.classList.add('carrousel__fleche');
+    carrousel__fleche__droite.classList.add('carrousel__fleche--droite');
+    carrousel__fleche__droite.innerHTML = '>';
+    
+    
+    
+    console.log(form);
+    
+    //appelé un fonction à partir du bouton de droite
+    carrousel__fleche__droite.addEventListener('click', function(){
+        let index = 0;
+        for (const radio of carrousel__radios) {
+            if(radio.checked){
+                radio.checked = false;
+                carrousel__imgs[index].style.opacity = 0;
+                if(index == carrousel__radios.length - 1){
+                    index = 0;
+                }else{
+                    index++;
+                }
+                carrousel__radios[index].checked = true;
+                carrousel__imgs[index].style.opacity = 1;
+                break;
+            }
+            index++;
+        }
+    });
+    
+    //appelé un fonction à partir du bouton de gauche
+    carrousel__fleche__gauche.addEventListener('click', function(){
+        let index = 0;
+        for (const radio of carrousel__radios) {
+            if(radio.checked){
+                radio.checked = false;
+                carrousel__imgs[index].style.opacity = 0;
+                if(index == 0){
+                    index = carrousel__radios.length - 1;
+                }else{
+                    index--;
+                }
+                carrousel__radios[index].checked = true;
+                carrousel__imgs[index].style.opacity = 1;
+                break;
+            }
+            index++;
+        }
+    }
+    
+    );
+    
+    
+    carrousel.appendChild(carrousel__fleche__droite);
+    carrousel.appendChild(carrousel__fleche__gauche);
+    
 
 
     function creer_radio_carrousel(index){
@@ -87,6 +171,11 @@ carrousel__x.addEventListener('mousedown', function(){
 
     console.log('fermer carrousel')
 })
+
+
+
+
+
 
 
 /*
